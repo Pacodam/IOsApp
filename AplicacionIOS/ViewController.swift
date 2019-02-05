cda69749875f32a97a5a60abf63dd9da56d55598
pacodam <pacodam2@gmail.com>
2019-02-05 19:47:32 +0100

    Initial Commit

---
+++ AplicacionIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AplicacionIOS
+//
+//  Created by Francisco Jose Moreno on 5/2/19.
+//  Copyright © 2019 Francisco Jose Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
